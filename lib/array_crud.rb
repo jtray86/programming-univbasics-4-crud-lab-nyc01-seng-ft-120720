ca5fda6465862f6da4095614f3c1d331c3077919
cheerful-callback-1955 <jenniferetracy@gmail.com>
2020-11-11 21:48:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,11 +26,14 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  arrays.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
 end
 
 def retrieve_first_element_from_array(array)
