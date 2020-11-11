82002b39fb2166783c4c2bcffb17083e3b0e740d
cheerful-callback-1955 <jenniferetracy@gmail.com>
2020-11-11 21:44:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,10 +17,11 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  array.unshift ("WOW")
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["I", "am", "really", "learning"]
   
 end
 
